98daeb02e2d242e02d1b52d8c10297de91cffb64
mustafinDamir2023 <mustafind@yandex.ru>
2025-02-07 13:11:24 +0000

    add demon script

---
+++ webpack-start.js
@@ -0,0 +1,14 @@
+import  { exec } from 'child_process'
+
+exec('webpack -d --mode development', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
